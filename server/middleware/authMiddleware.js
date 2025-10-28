ce129af59f7cd5742d38ed0bfa5708dc9a7aaef3
dagm <dagimyibabe19@gmail.com>
2025-10-28 18:39:30 +0300

    feat: implement JWT authentication middleware for token verification

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+export default (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+        req.body.userId = decodedToken.userId;
+
+        next();
+    } catch (error) {
+        res.send({
+            message: error.message,
+            success: false,
+        });
+    }
+};
