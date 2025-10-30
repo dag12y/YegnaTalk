ca34ec3fca30237a4f98147fe7df987543571d1d
dagm <dagimyibabe19@gmail.com>
2025-10-30 14:56:12 +0300

    feat: add getAllUsers controller and update user router to include new endpoint

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -2,6 +2,7 @@ import User from "./../models/userModel.js";
 
 export async function getLoggedUser(req, res) {
     try {
+        //get the logged user
         const user = await User.findOne({ _id: req.body.userId });
 
         res.send({
@@ -10,6 +11,25 @@ export async function getLoggedUser(req, res) {
             data:user
         })
 
+    } catch (error) {
+        res.send({
+            message: error.message,
+            success: false,
+        });
+    }
+}
+
+export async function getAllUsers(req,res) {
+    try {
+        //getting all users except the logged user
+        const userId=req.body.userId
+        const allUsers=await User.find({_id:{$ne:{userId}}})
+
+         res.send({
+            message:"All users fetched successfully",
+            success:true,
+            data:allUsers
+        })
     } catch (error) {
         res.send({
             message: error.message,
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getLoggedUser } from '../controllers/userControllers.js';
+import { getLoggedUser,getAllUsers } from '../controllers/userControllers.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/get-logged-user',authMiddleware,getLoggedUser)
+userRouter.get('/get-all-users',authMiddleware,getAllUsers)
 
 export default userRouter;
