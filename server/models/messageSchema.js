ee9deef8f4f5249d2ae3f0afa73e26a22ac68d01
dagm <dagimyibabe19@gmail.com>
2025-10-31 14:54:02 +0300

    feat: create message schema for chat messages with sender, text, and read status

---
+++ server/models/messageSchema.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const messageSchema = mongoose.Schema({
+    chatId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "chats",
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    read: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+export default mongoose.model("messages", messageSchema);
