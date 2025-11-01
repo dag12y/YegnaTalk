f1e84a2be5d68b2741f20b54d90077ff03e31934
dagm <dagimyibabe19@gmail.com>
2025-11-01 12:00:56 +0300

    feat: enhance message functionality by adding getAllMessages endpoint and improving code formatting

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -1,7 +1,7 @@
-import Message from './../models/messageModel.js'
-import Chats from './../models/chatModel.js'
+import Message from "./../models/messageModel.js";
+import Chats from "./../models/chatModel.js";
 
-export async function newMessage(req,res) {
+export async function newMessage(req, res) {
     try {
         //store the message in the db
         const newMessage = new Message(req.body);
@@ -10,27 +10,48 @@ export async function newMessage(req,res) {
 
         //update lastmessage in chat application
 
-        // const currentChat = await Chats.findById(req.body.chatId) 
+        // const currentChat = await Chats.findById(req.body.chatId)
         // currentChat.lastMessage = savedMessage._id;
         // await currentChat.save()
 
-        const currentChat = await Chats.findOneAndUpdate({
-            _id:req.body.chatId
-        },{
-            lastMessage:savedMessage._id,
-            $inc:{unreadMessageCount:1}
-        })
+        const currentChat = await Chats.findOneAndUpdate(
+            {
+                _id: req.body.chatId,
+            },
+            {
+                lastMessage: savedMessage._id,
+                $inc: { unreadMessageCount: 1 },
+            }
+        );
 
         res.status(201).send({
-            message:"Message sent successfully",
-            success:true,
-            data:savedMessage
-        })
-
+            message: "Message sent successfully",
+            success: true,
+            data: savedMessage,
+        });
     } catch (error) {
         res.status(400).send({
-            message:error.message,
-            success:false
-        })
+            message: error.message,
+            success: false,
+        });
+    }
+}
+
+export async function getAllMessages(req, res) {
+    try {
+        const allMessages = await Message.find({
+            chatId: req.params.chatId,
+        }).sort({ createdAt: 1 });
+
+        res.status(200).send({
+            message: "all message fetched successfully",
+            success: true,
+            data: allMessages,
+        });
+    } catch (error) {
+        res.status(400).send({
+            message: error.message,
+            success: false,
+        });
     }
 }
--- server/routers/messageRouter.js
+++ server/routers/messageRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import authMiddleware from "../middleware/authMiddleware.js";
-import { newMessage} from '../controllers/messageControllers.js ';
+import { newMessage,getAllMessages} from '../controllers/messageControllers.js ';
 
 const messageRouter = express.Router()
 
 messageRouter.post('/new-message',authMiddleware,newMessage)
+messageRouter.get('/get-all-messages/:chatId',authMiddleware,getAllMessages)
 
 
 export default messageRouter;
