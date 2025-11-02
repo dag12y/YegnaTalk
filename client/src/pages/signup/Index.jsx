86a046ccf042128c917a6ec81037db0fca1f87e1
dagm <dagimyibabe19@gmail.com>
2025-11-02 19:30:05 +0300

    Refactor code structure for  signup page

--- client/src/pages/signup/Index.jsx
+++ client/src/pages/signup/Index.jsx
@@ -1,5 +1,30 @@
 export default function Signup() {
   return (
-    <div>this is Signup page</div>
-  )
+      <div className="container">
+          <div className="container-back-img"></div>
+          <div className="container-back-color"></div>
+          <div className="card">
+              <div className="card_title">
+                  <h1>Create Account</h1>
+              </div>
+              <div className="form">
+                  <form>
+                      <div className="column">
+                          <input type="text" placeholder="First Name" autoComplete="off"/>
+                          <input type="text" placeholder="Last Name" autoComplete="off"/>
+                      </div>
+                      <input type="email" placeholder="Email" autoComplete="off"/>
+                      <input type="password" placeholder="Password" autoComplete="off"/>
+                      <button>Sign Up</button>
+                  </form>
+              </div>
+              <div className="card_terms">
+                  <span>
+                      Already have an account?
+                      <a>Login Here</a>
+                  </span>
+              </div>
+          </div>
+      </div>
+  );
 }
