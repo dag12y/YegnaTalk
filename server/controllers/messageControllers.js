6a212e6519bf0af6b1b51d1c72982effb338f0d0
dagm <dagimyibabe19@gmail.com>
2025-11-02 12:36:59 +0300

    refactor: improve comments and variable usage in message controller functions and allow only to get chat that is in the member

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -3,7 +3,7 @@ import Chats from "./../models/chatModel.js";
 
 export async function newMessage(req, res) {
     try {
-        //store the message in the db
+        //create and store the message in the db
         const newMessage = new Message(req.body);
 
         const savedMessage = await newMessage.save();
@@ -39,9 +39,39 @@ export async function newMessage(req, res) {
 
 export async function getAllMessages(req, res) {
     try {
+        //get user id
+        const userId = req.body.userId
+        const chatId =req.params.chatId
+
+        //check if it exists
+
         const allMessages = await Message.find({
-            chatId: req.params.chatId,
+            chatId: chatId,
         }).sort({ createdAt: 1 });
+
+
+        const chat = await Chats.findById(req.params.chatId);
+
+        if (!chat) {
+            return res.status(404).send({
+                message: "Chat not found",
+                success: false,
+            });
+        }
+
+        // making sure the user is a member of this chat
+        const isMember = chat.members.some(
+            (memberId) => memberId.toString() === req.body.userId.toString()
+        );
+
+        if (!isMember) {
+            return res.status(403).send({
+                message: "Access denied: You are not a member of this chat",
+                success: false,
+            });
+        }
+
+
 
         res.status(200).send({
             message: "all message fetched successfully",
