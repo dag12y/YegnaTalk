3ccb886e4a64ac383727ccad5843df1b386a74d6
dagm <dagimyibabe19@gmail.com>
2025-11-02 19:53:12 +0300

    feat: implement login page with controlled components and form submission

--- client/src/pages/login/Index.jsx
+++ client/src/pages/login/Index.jsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+
 export default function Login() {
-    return <div>this is Login page</div>;
+    const [user,setUser]=useState({
+        email:'',
+        password:''
+    })
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(user);
+        
+    }
+
+    return (
+        <div className="container">
+            <div className="container-back-img"></div>
+            <div className="container-back-color"></div>
+            <div className="card">
+                <div className="card_title">
+                    <h1>Login Here</h1>
+                </div>
+                <div className="form">
+                    <form onSubmit={handleSubmit}>
+                        <input type="email" placeholder="Email" value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})}/>
+                        <input type="password" placeholder="Password" value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})}/>
+                        <button>Login</button>
+                    </form>
+                </div>
+                <div className="card_terms">
+                    <span>
+                        Don't have an account yet?
+                        <a>Signup Here</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
 }
