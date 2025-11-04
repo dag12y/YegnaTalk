79e45c76be9506d9049a5bc463344a654f246185
dagm <dagimyibabe19@gmail.com>
2025-11-04 13:26:02 +0300

    feat: integrate Redux for state management with loader functionality

---
+++ client/src/redux/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit'
+import loaderReducer from './loaderSlice.js'
+
+const store = configureStore(
+    {
+        reducer:{loaderReducer}
+    }
+)
+
+export default store;
