5285b15eb8c0ff2f3bd3121dd27e7e0a053e852f
dagm <dagimyibabe19@gmail.com>
2025-11-07 13:27:12 +0300

    feat: fetch all messages for selected chat

---
+++ client/src/api/message.js
@@ -0,0 +1,24 @@
+import { axiosInstance } from "./index";
+
+export async function createNewMessage(message) {
+    try {
+        const response = await axiosInstance.post(
+            "/api/message/new-message",
+            message
+        );
+        return response.data;
+    } catch (error) {
+        return error;
+    }
+}
+
+export async function getAllMessages(chatId) {
+    try {
+        const response = await axiosInstance.get(
+            `api/message/get-all-messages/${chatId}`
+        );
+        return response.data;
+    } catch (error) {
+        return error;
+    }
+}
--- client/src/pages/home/components/ChatArea.jsx
+++ client/src/pages/home/components/ChatArea.jsx
@@ -1,21 +1,94 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { createNewMessage, getAllMessages } from "../../../api/message";
+import { showLoader, hideLoader } from "./../../../redux/loaderSlice";
+import { toast } from "react-hot-toast";
+import { useEffect, useState } from "react";
 
 export default function ChatArea() {
-    const { selectedChat,user } = useSelector((state) => state.userReducer);
-    const selectedUser = selectedChat.members.find(u=>u._id!==user._id);
+    const { selectedChat, user } = useSelector((state) => state.userReducer);
+    const selectedUser = selectedChat.members.find((u) => u._id !== user._id);
+    const dispatch = useDispatch();
+    const [message, setMessage] = useState("");
+    const [allMessage, setAllMessage] = useState("");
+
+    async function sendMessage() {
+        try {
+            if (!message.trim()) {
+                toast.error("Message cannot be empty");
+                return;
+            }
+
+            const formattedMessage = {
+                chatId: selectedChat._id,
+                sender: user._id,
+                text: message,
+            };
+
+            dispatch(showLoader());
+            const response = await createNewMessage(formattedMessage);
+            dispatch(hideLoader());
+
+            if (response.success) {
+                toast.success("Message sent!");
+                setMessage("");
+            } else {
+                toast.error(response.message);
+            }
+        } catch (error) {
+            dispatch(hideLoader());
+            toast.error(error.message);
+        }
+    }
+
+    async function getMessages() {
+        try {
+            dispatch(showLoader());
+            const response = await getAllMessages(selectedChat._id)
+            dispatch(hideLoader());
+
+            if (response.success) {
+                setAllMessage(response.data);
+            } else {
+                toast.error(response.message);
+            }
+        } catch (error) {
+            dispatch(hideLoader());
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(()=>{
+        getMessages()
+    },[selectedChat])
+
     return (
         <>
             {selectedChat && (
                 <div className="app-chat-area">
                     <div className="app-chat-area-header">
-                        {selectedUser.firstname + ' '+ selectedUser.lastname}
+                        {selectedUser.firstname + " " + selectedUser.lastname}
                     </div>
-                    <div>
-                        CHAT AREA
-                    </div>
-                    <div>
-                         SEND MESSAGE 
-                    </div>
+                    <div className="main-chat-area">CHAT AREA</div>
+                    <div className="send-message-div">
+                        <textarea
+                            className="send-message-input"
+                            placeholder="Type a message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                            rows={1}
+                            onInput={(e) => {
+                                e.target.style.height = "auto";
+                                e.target.style.height =
+                                    e.target.scrollHeight + "px";
+                            }}
+                        />
+
+                        <button
+                            className="fa fa-paper-plane send-message-btn"
+                            aria-hidden="true"
+                            onClick={sendMessage}
+                        ></button>
+                    </div>{" "}
                 </div>
             )}
         </>
