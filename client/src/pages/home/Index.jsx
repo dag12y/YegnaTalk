044ca25afc5102cf6537f84b3981a03d62b5949d
dagm <dagimyibabe19@gmail.com>
2025-11-10 20:44:02 +0300

    feat: add socket.io client and server connection setup

--- client/src/pages/home/Index.jsx
+++ client/src/pages/home/Index.jsx
@@ -3,9 +3,12 @@ import "../../css/home.css";
 import ChatArea from "./components/ChatArea";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
+import {io} from 'socket.io-client'
 
 export default function Home() {
     const {selectedChat} = useSelector(state=>state.userReducer);
+    const socket = io('http://localhost:5000')
+
     return (
         <div className="home-page">
             <Header />
--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ import userRouter from "./routers/userRouter.js";
 import chatRouter from "./routers/chatRouter.js";
 import messageRouter from "./routers/messageRouter.js";
 import verifyRouter from "./routers/verificationRouter.js";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
 dotenv.config();
 
 // Connect to DB
@@ -14,18 +17,36 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//middleware
+// Middleware
 app.use(express.json());
 
-//routers
+// Create HTTP server from Express app
+const server = createServer(app); 
+
+const io = new Server(server, {
+    // instantiate socket.io server
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
+
+// Routers
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/chat", chatRouter);
-app.use('/api/message',messageRouter)
-app.use('/api',verifyRouter)
+app.use("/api/message", messageRouter);
+app.use("/api", verifyRouter);
 
-app.listen(PORT, () => {
-    console.log("Server is running on port:", PORT);
+// Socket.io connection
+io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Socket disconnected:", socket.id);
+    });
 });
 
-
+server.listen(PORT, () => {
+    console.log(" Server running on port:", PORT);
+});
