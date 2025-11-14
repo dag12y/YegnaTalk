cbe974949d50c7fbc0bdd4527c747170870e56bd
dagm <dagimyibabe19@gmail.com>
2025-11-14 13:44:20 +0300

    feat: implement profile picture upload functionality using Cloudinary

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import User from "./../models/userModel.js";
+import cloudinary from "../configs/cloudinary.js";
 
 export async function getLoggedUser(req, res) {
     try {
@@ -34,5 +35,33 @@ export async function getAllUsers(req,res) {
             message: error.message,
             success: false,
         });
+    }
+}
+
+export async function uploadProfilePic(req,res) {
+    try {
+        const image = req.body.image;
+
+        //upload image to cloudinary
+        const uploadedImage = await cloudinary.uploader.upload(image,{
+            folder:'YegnaTalk',
+        })
+        //update the user model and set profile property
+        const user = await User.findByIdAndUpdate(
+            {_id:req.body.userId},
+            {profile:uploadedImage.secure_url},
+            {new:true}
+        )
+
+        res.status(201).send({
+            message:'Profile picture uploaded successfully.',
+            success:true,
+            data:user
+        })
+    } catch (error) {
+        res.status(400).send({
+            message:error.message,
+            success:false
+        })
     }
 }
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getLoggedUser,getAllUsers } from '../controllers/userControllers.js';
+import { getLoggedUser,getAllUsers,uploadProfilePic } from '../controllers/userControllers.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/get-logged-user',authMiddleware,getLoggedUser)
 userRouter.get('/get-all-users',authMiddleware,getAllUsers)
+userRouter.post('/upload-profile-pic',authMiddleware,uploadProfilePic)
 
 export default userRouter;
